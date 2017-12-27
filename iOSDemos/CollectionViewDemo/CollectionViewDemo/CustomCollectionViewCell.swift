640f1113acefddc104a7b919230912c09d974398
Varun <varun.kudalkar@gmail.com>
2017-12-27 17:09:54 +0530

    Collection view demo

---
+++ iOSDemos/CollectionViewDemo/CollectionViewDemo/CustomCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomCollectionViewCell.swift
+//  CollectionViewDemo
+//
+//  Created by Varun on 27/12/17.
+//  Copyright © 2017 Codekul. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var lbl : UILabel!
+    
+    
+}
---
+++ iOSDemos/CollectionViewDemo/CollectionViewDemo/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  CollectionViewDemo
+//
+//  Created by Varun on 27/12/17.
+//  Copyright © 2017 Codekul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+     
+        return 10
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CustomCollectionViewCell
+        
+        cell.lbl.text = "\(indexPath.row + 1)"
+        
+        return cell
+        
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        print("section: \(indexPath.section)\nrow: \(indexPath.row)")
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
